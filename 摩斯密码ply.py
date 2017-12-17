a1a4d36bcded25b392f70da6e737c2035b310fe7
Winston Pan <33798377+Winston-Pan@users.noreply.github.com>
2017-12-17 13:28:12 +0800

    Add files via upload

---
+++ 摩斯密码ply.py
@@ -0,0 +1,35 @@
+name=raw_input("what is your name?")
+print "hi",name
+import RPi.GPIO as gpio
+import time
+
+gpio.setmode(gpio.BOARD)
+gpio.setup(7,gpio.OUT)
+
+gpio.output(7,gpio.HIGH)
+time.sleep(0.1)
+gpio.output(7,gpio.LOW)
+time.sleep(0.1)
+gpio.output(7,gpio.HIGH)
+time.sleep(0.1)
+gpio.output(7,gpio.LOW)
+time.sleep(0.1)
+gpio.output(7,gpio.HIGH)
+time.sleep(0.1)
+gpio.output(7,gpio.LOW)
+time.sleep(0.1)
+gpio.output(7,gpio.HIGH)
+time.sleep(0.1)
+gpio.output(7,gpio.LOW)
+time.sleep(0.3)
+
+gpio.output(7,gpio.HIGH)
+time.sleep(0.1)
+gpio.output(7,gpio.LOW)
+time.sleep(0.1)
+gpio.output(7,gpio.HIGH)
+time.sleep(0.1)
+gpio.output(7,gpio.LOW)
+time.sleep(0.3)
+
+gpio.cleanup()
