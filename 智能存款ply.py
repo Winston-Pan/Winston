2519fa8798f2c300a9a0d8ec0978f9c4ce4a956b
Winston Pan <33798377+Winston-Pan@users.noreply.github.com>
2017-12-18 14:25:34 +0800

    Add files via upload

---
+++ 智能存款ply.py
@@ -0,0 +1,24 @@
+username="12345"
+username=raw_input("please enter your username:")
+if username == username:
+    print "the username is right."
+else:
+    print "the username is wrong,please enter your username again."
+username=raw_input("please enter your username:"
+if username == username:
+    print "the username is right."
+else:
+    print "the username is wrong again,you can not try again."
+cleanup()
+passward="6789"
+password=raw_input("please enter your passwold:")
+if password == password:
+    print "the password is right.you have $1000000000."
+else:
+    print "the passwold is wrong,please try it again."
+password=raw_input("please enter your passwold:")
+if password == passwold:
+    print "the password is right.you have $1000000000."
+else:
+    print "the passwold is wrong,you can not try again."
+cleanup()
